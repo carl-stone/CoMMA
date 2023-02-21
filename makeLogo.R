80d010862f29e4c5e5d6ded6198572121754d1e0
Carl Stone <carl.j.stone@vanderbilt.edu>
2023-02-21 15:58:46 -0600

    add logo

---
+++ makeLogo.R
@@ -0,0 +1,13 @@
+sticker(imgurl,
+        package = "CoMMA",
+        p_color = "#FC8EAC",
+        p_size = 24,
+        s_x = 1,
+        s_y = 1,
+        s_height = 0.5,
+        h_color = "#FC8EAC",
+        h_fill = "white",
+        white_around_sticker = TRUE,
+        dpi = 600,
+        filename =
+          "/Users/carlstone/Library/CloudStorage/Box-Box/Behringer_Lab_Box_Drive/Projects/LongTermExpEvo/MethylationProject/Rscripts/comma_logo.png")
