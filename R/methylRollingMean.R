829c113d5822b9fbb1f6cc01bf5a605dd445eeba
carl-stone <41388958+carl-stone@users.noreply.github.com>
2023-03-15 16:44:20 -0500

    Add documentation to methylRollingMean.R

--- R/methylRollingMean.R
+++ R/methylRollingMean.R
@@ -1,3 +1,23 @@
+#' Calculate rolling mean of methylation levels
+#'
+#' Calculates the rolling mean of methylation levels for a given window size. The function takes in a data frame with columns
+#' for genomic position and methylation level, and returns a data frame with columns for position and the rolling mean
+#' methylation level for that position.
+#'
+#' @param df A data frame with columns for genomic position and methylation level.
+#' @param position_col The name of the column in \code{df} that contains the genomic position data.
+#' @param methyl_col The name of the column in \code{df} that contains the methylation level data.
+#' @param w_size The window size for calculating the rolling mean. Default value is 1000.
+#' @param genome_size The size of the genome. Default value is 4641652.
+#' @param verbose If \code{TRUE}, progress updates will be printed. Default value is \code{FALSE}.
+#' 
+#' @return A data frame with columns for genomic position and the rolling mean methylation level for that position.
+#' 
+#' @examples
+#' df <- data.frame(position = c(1, 2, 3, 4, 5), methyl = c(0.1, 0.2, 0.3, 0.4, 0.5))
+#' result <- methylRollingMean(df, position_col = "position", methyl_col = "methyl", w_size = 2)
+#' 
+#' @import dplyr
 methylRollingMean <- function(df, position_col, methyl_col, w_size=1000, genome_size=4641652, verbose=FALSE) {
   tstart <- Sys.time()
   # Take the first w_size of sites from the beginning of the chromosome and add them to the end so it wraps around
