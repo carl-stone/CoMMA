7db38951e03b1374cf3c29232df0d33a1b057d26
carl-stone <41388958+carl-stone@users.noreply.github.com>
2026-02-23 15:04:34 -0600

    Harden rolling methylation helpers with validation and sorting

--- R/methylRollingMean.R
+++ R/methylRollingMean.R
@@ -1,15 +1,27 @@
-methylRollingMean <- function(df, position_col, methyl_col, w_size=1000, genome_size=4641652, verbose=FALSE) {
+methylRollingMean <- function(df,
+                              position_col,
+                              methyl_col,
+                              w_size = 1000,
+                              genome_size = 4641652,
+                              method = "fast",
+                              verbose = FALSE) {
   legacy_soft_deprecate("methylRollingMean", "methylRollingMedian")
+  if (!is.character(method) || length(method) != 1L || is.na(method) || method != "fast") {
+    stop("`method` must be 'fast'.", call. = FALSE)
+  }
+  if (!is.logical(verbose) || length(verbose) != 1L || is.na(verbose)) {
+    stop("`verbose` must be a single TRUE/FALSE value.", call. = FALSE)
+  }
   tstart <- Sys.time()
   # Take the first w_size of sites from the beginning of the chromosome and add them to the end so it wraps around
-  df <- dplyr::bind_cols(position = df[[position_col]], methyl = df[[methyl_col]])
+  df <- .validate_rolling_input(df, position_col, methyl_col, w_size, genome_size)
   nsites <- nrow(df)
   beginning_sites <- df |>
     dplyr::filter(position <= w_size) |>
     dplyr::mutate(position = position + genome_size)
-  df <- dplyr::bind_rows(df, beginning_sites)
+  df <- dplyr::bind_rows(df, beginning_sites) |>
+    dplyr::arrange(position)
   df <- data.matrix(df)
-  # TODO sort df by position
   out_df <- data.frame(position = as.numeric(rep(NA, nsites)),
                    mean_methyl = as.double(rep(NA, nsites)))
   out_df <- data.matrix(out_df)
