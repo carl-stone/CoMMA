ee5a1c8af93f831fb07f9e47d0a3144fae5bc969
carl-stone <41388958+carl-stone@users.noreply.github.com>
2026-02-23 13:43:08 -0600

    Harden annotation helpers with validation and namespace-safe calls

--- R/calculateMethylSiteDepth.R
+++ R/calculateMethylSiteDepth.R
@@ -1,9 +1,9 @@
 calculateMethylSiteDepth <- function(df, position_col, cov_col, w_size, calc_log2 = FALSE) {
-  out_df <- tibble(position = integer(),
-                   coverage = double())
+  out_df <- tibble::tibble(position = integer(),
+                           coverage = double())
   for (i in 1:ceiling(max(df[[position_col]])/w_size)) {
     out_df[i,1] <- i*w_size
-    out_df[i,2] <- mean(df[between(df[[position_col]], (i-1)*w_size-1, i*w_size), cov_col][[1]])
+    out_df[i,2] <- mean(df[dplyr::between(df[[position_col]], (i-1)*w_size-1, i*w_size), cov_col][[1]])
   }
   if (calc_log2 == TRUE) {
     out_df$log2_coverage <- log2(out_df$coverage)
